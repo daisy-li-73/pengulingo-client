1dd894900a066885856153f2f39ee31ca751827c
Selena Zhou <58157434+zhouselena@users.noreply.github.com>
2024-05-24 21:03:15 -0400

    restructure landing page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import {
-  BrowserRouter, Routes, Route, NavLink,
+  BrowserRouter, Routes, Route,
 } from 'react-router-dom';
 import CreateGame from './CreateGame';
+import LandingScreen from './landing-page/loadingscreen';
 
 function FallBack(props) {
   return <div>URL Not Found</div>;
 }
 
-function NavBar(props) {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/creategame">Create Game</NavLink>
-        </li>
-        <li>
-          <NavLink to="/joingame">Join Game</NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+// function NavBar(props) {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>
+//           <NavLink to="/">Home</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/creategame">Create Game</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/joingame">Join Game</NavLink>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
 
 function Test(props) {
   return <div>Testing page for now</div>;
@@ -35,7 +36,7 @@ function App() {
     <BrowserRouter>
       <div>
         <Routes>
-          <Route path="/" element={<NavBar />} />
+          <Route path="/" element={<LandingScreen />} />
           <Route path="/creategame" element={<CreateGame />} />
           <Route path="/joingame" element={<Test />} />
           <Route path="/joingame/:roomID" element={<Test />} />
---
+++ src/components/landing-page/loadingscreen.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+function LoadingScreen() {
+  const [language, setLanguage] = useState('French');
+
+  const handleLanguageChange = (selectedLanguage) => {
+    setLanguage(selectedLanguage);
+  };
+
+  return (
+    <div>
+      <div className="header-container">
+        <div className="logo-container">
+          <NavLink to="/"><img src="src/components/landing-page/images/pengulingo.png" alt="logo" /></NavLink>
+        </div>
+        <div className="log-in-container">
+          <NavLink to="/"><img src="src/components/landing-page/images/log-in.png" alt="log-in" /></NavLink>
+        </div>
+      </div>
+      <div className="image-container">
+        <div className="pengu-container">
+          <img src="src/components/landing-page/images/pengu-loading-screen.png" className="pengu" />
+          <div className="balloons">
+            <img src="src/components/landing-page/images/balloon-green.png" className="balloon-green" />
+            <img src="src/components/landing-page/images/balloon-pink.png" className="balloon-pink" />
+            <img src="src/components/landing-page/images/balloon-yellow.png" className="balloon-yellow" />
+            <img src="src/components/landing-page/images/balloon-red.png" className="balloon-red" />
+          </div>
+        </div>
+      </div>
+      <div className="globe-container">
+        <img src="src/components/landing-page/images/globe.png" alt="globe" />
+      </div>
+      <div className="join-game">
+        <NavLink to="/joingame"><img src="src/components/landing-page/images/join-game.png" alt="Join Game" className="join-game-img" /></NavLink>
+        <NavLink to="/joingame"><img src="src/components/landing-page/images/join-game-hover.png" alt="Join Hover" className="join-game-hover" /></NavLink>
+      </div>
+      <div className="host-game">
+        <NavLink to="/creategame"><img src="src/components/landing-page/images/host-game.png" alt="Host Game" className="host-game-img" /></NavLink>
+        <NavLink to="/creategame"><img src="src/components/landing-page/images/host-game-hover.png" alt="Host Hover" className="host-game-hover" /></NavLink>
+      </div>
+      <div className="text">
+        <img src="src/components/landing-page/images/display-text.png" alt="display-text" />
+      </div>
+      <div className="language-selector">
+        <button className={language === 'French' ? 'selected' : ''} onClick={() => handleLanguageChange('French')}><img src="src/components/landing-page/images/French.png" alt="French" /></button>
+        <button className={language === 'Chinese' ? 'selected' : ''} onClick={() => handleLanguageChange('Chinese')}><img src="src/components/landing-page/images/Chinese.png" alt="Chinese" /></button>
+        <button className={language === 'Italian' ? 'selected' : ''} onClick={() => handleLanguageChange('Italian')}><img src="src/components/landing-page/images/Italian.png" alt="Italian" /></button>
+        <button className={language === 'Spanish' ? 'selected' : ''} onClick={() => handleLanguageChange('Spanish')}><img src="src/components/landing-page/images/Spanish.png" alt="Spanish" /></button>
+      </div>
+    </div>
+  );
+}
+
+export default LoadingScreen;
