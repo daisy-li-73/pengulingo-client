0dafdaec93368d6f87636361a7b619972ddc94e7
Selena Zhou <58157434+zhouselena@users.noreply.github.com>
2024-05-28 15:40:39 -0400

    added new questions and more styling

--- src/components/GroceryGame.jsx
+++ src/components/GroceryGame.jsx
@@ -10,15 +10,30 @@ import broccoli from '../img/broccoli.png';
 import baguette from '../img/baguette.png';
 import coffeebeans from '../img/coffeebeans.png';
 import strawberry from '../img/strawberry.png';
+import carrot from '../img/carrot.png';
+import celery from '../img/celery.png';
+import cow from '../img/cow.png';
+import lobster from '../img/lobster.png';
+import tomato from '../img/tomato.png';
+import wheat from '../img/wheat.png';
 import unchecked from '../img/unchecked.png';
 import correctBg from '../img/foodcorrectbg.png';
 import incorrectBg from '../img/foodincorrectbg.png';
+import speechbubble from '../img/speechbubble.png';
+import checked from '../img/checked.png';
+import wrong from '../img/wrong.png';
 
 const images = {
   broccoli,
   baguette,
   coffeebeans,
   strawberry,
+  carrot,
+  celery,
+  cow,
+  lobster,
+  wheat,
+  tomato,
 };
 
 function GroceryGame(props) {
@@ -31,7 +46,7 @@ function GroceryGame(props) {
   };
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [correctCount, setCorrectCount] = useState(0);
-  const [currentCorrect, setCurrentCorrect] = useState(false);
+  const [currentCorrect, setCurrentCorrect] = useState(0); // -1:incorrect, 0: nothing, 1:correct
   const [showModal, setShowModal] = useState(false);
   const [reachedEnd, setReachedEnd] = useState(false);
 
@@ -39,15 +54,17 @@ function GroceryGame(props) {
     console.log('outcome', outcome);
     if (outcome === 'correct') {
       setCorrectCount(correctCount + 1);
-      setCurrentCorrect(true);
+      setCurrentCorrect(1);
       console.log('added correct', correctCount);
+    } else {
+      setCurrentCorrect(-1);
     }
     setShowModal(true);
     setTimeout(() => {
       setShowModal(false);
       if (currentQuestionIndex < data.questions.length - 1) {
         setCurrentQuestionIndex(currentQuestionIndex + 1);
-        setCurrentCorrect(false);
+        setCurrentCorrect(0);
       } else {
         setReachedEnd(true);
       }
@@ -66,6 +83,15 @@ function GroceryGame(props) {
   };
 
   const currentQuestion = data.questions[currentQuestionIndex];
+  const questionStatus = () => {
+    if (currentCorrect === -1) {
+      return wrong;
+    } else if (currentCorrect === 0) {
+      return unchecked;
+    } else {
+      return checked;
+    }
+  };
 
   return (
     <div className="grocery-game-page">
@@ -73,7 +99,7 @@ function GroceryGame(props) {
       {!reachedEnd && (
         <div className="grocery-questions">
           <div className="grocery-question-text">
-            <img src={unchecked} />
+            <img src={questionStatus()} />
             <p>Je cherche</p>
             <p className="grocery-question-textdeco">
               {currentQuestion.question}.
@@ -104,11 +130,14 @@ function GroceryGame(props) {
                   </button>
                 ))}
               </div>
-              <p>
-                {currentCorrect
-                  ? data.outcomes.correct.text
-                  : data.outcomes.incorrect.text}
-              </p>
+              <div className="grocery-correct-modal-textbg">
+                <p className="speechbubble-text">
+                  {currentCorrect === 1
+                    ? data.outcomes.correct.text
+                    : data.outcomes.incorrect.text}
+                </p>
+                <img src={speechbubble} className="speechbubble-image" />
+              </div>
             </div>
           )}
         </div>
