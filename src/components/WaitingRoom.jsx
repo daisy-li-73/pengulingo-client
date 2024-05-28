1080b7685cb531d62eac76516ffabdde5333f17e
Selena Zhou <58157434+zhouselena@users.noreply.github.com>
2024-05-27 22:52:21 -0400

    full frontend for waiting room

--- src/components/WaitingRoom.jsx
+++ src/components/WaitingRoom.jsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import PageTopBar from './PageTopBar';
 import smallloadingicon from '../img/smallloadingicon.png';
+import loadingcircle from '../img/loading_circle.png';
+import pengu from '../img/pengu_happy.png';
 
 // change props to usestore
 function WaitingRoom(props) {
-  const player1Name = props.player1Name || 'Selenaaaa';
+  const player1Name = props.player1Name || 'Selena';
   const player2Name = props.player2Name || '';
   const player3Name = props.player3Name || '';
   const player4Name = props.player4Name || '';
@@ -47,14 +49,34 @@ function WaitingRoom(props) {
   return (
     <div className="waiting-room-page">
       <PageTopBar />
-      <div className="display-players">
-        {playerBar('#E32222', '#EE6363', player1Name, true)}
-        {playerBar('#28B3DF', '#60CBED', player2Name, false)}
+      <div className="foreground-waitingroom">
+        <div className="code">
+          <img src={pengu} alt="pengu logo" className="pengu-logo" />
+          <div className="code-text">
+            {Array.from(code).map((letter, index) => (
+              <div className="code-circle">{letter}</div>
+            ))}
+          </div>
+          <h2>
+            Share this code with
+            <br /> your friends!
+          </h2>
+        </div>
+        <div className="display-all-players">
+          <div className="display-players">
+            {playerBar('#E32222', '#EE6363', player1Name, true)}
+            {playerBar('#28B3DF', '#60CBED', player2Name, false)}
+          </div>
+          <div className="display-players">
+            {playerBar('#EFD910', '#FFE76C', player3Name, true)}
+            {playerBar('#27DE2E', '#62F370', player4Name, false)}
+          </div>
+        </div>
       </div>
-      <div className="code">{code}</div>
-      <div className="display-players">
-        {playerBar('#EFD910', '#FFE76C', player3Name, true)}
-        {playerBar('#27DE2E', '#62F370', player4Name, false)}
+      <div className="background-waitingroom">
+        <div className="loading-circle-div">
+          <img src={loadingcircle} alt="loading" className="big-loading" />
+        </div>
       </div>
     </div>
   );
