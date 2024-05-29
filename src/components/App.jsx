6fb7251e43198d9ad111633956d688cbf36d3d25
Jamie Nicholson <jnich999@yahoo.com>
2024-05-29 00:55:16 -0400

    choose game screen added!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable import/no-unresolved */
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { ToastContainer } from 'react-toastify';
 import CreateGame from './CreateGame';
 import LandingScreen from './landing-page/landingpage';
@@ -10,10 +8,9 @@ import LetsGo from './lets-go/lets-go';
 import LoadingPage from './loading-page/loading-screen';
 import WaitingRoom from './WaitingRoom';
 import ChooseGame from './choose-game/choose-game';
-import GoGoGroceries from './go-go-groceries/groceries-background';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import 'react-toastify/dist/ReactToastify.css';
 import GroceryGame from './GroceryGame';
+import GoGoGroceries from './go-go-groceries/groceries-background';
 import GroceryEndScreen from './GroceryEndScreen';
 import Test from './Test';
 
@@ -36,10 +33,10 @@ function App() {
           <Route path="/letsgo" element={<LetsGo />} />
           <Route path="/loading" element={<LoadingPage />} />
           <Route path="/choosegame" element={<ChooseGame />} />
-          <Route path="/gogogroceries" element={<GoGoGroceries />} />
           <Route path="/room/:roomID" element={<WaitingRoom />} />
           <Route path="/room/:roomID/choosegame" element={<ChooseGame />} />
           <Route path="/room/:roomID/:gameID" element={<GroceryGame />} />
+          <Route path="/gogogroceries" element={<GoGoGroceries />} />
           <Route
             path="/room/:roomID/:gameID/end"
             element={<GroceryEndScreen />}
