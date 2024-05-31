e9b22a2d4f5874d117221c559697ffa1d89adb0b
Selena Zhou <58157434+zhouselena@users.noreply.github.com>
2024-05-31 17:47:43 -0400

    added styling for choose game

--- src/components/choose-game/choose-game.jsx
+++ src/components/choose-game/choose-game.jsx
@@ -1,46 +1,75 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable indent */
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import PageTopBar from '../PageTopBar';
+import backgroundimg from './choose-game-images/background.png';
+import gogogroceriesselected from './choose-game-images/Go-Groceries-Selected.png';
+import directionsunselected from './choose-game-images/Directions-Unselected.png';
+import orderlychaosunselected from './choose-game-images/Orderly-Chaos-Unselected.png';
+import backbutton from '../../img/backbutton.png';
+import redplayer from './choose-game-images/red-player.png';
+import yellowplayer from './choose-game-images/yellow-player.png';
+import blueplayer from './choose-game-images/blue-player.png';
+import greenplayer from './choose-game-images/green-player.png';
+import penguhappy from './choose-game-images/Pengu Happy.png';
+import selectgame from './choose-game-images/select-game.png';
+import groceriesdescription from './choose-game-images/groceries-description.png';
 
 function ChooseGame() {
-    const [activeGame, setActiveGame] = useState(null);
+  const [activeGame, setActiveGame] = useState(null);
+  const navigate = useNavigate();
 
-    const handleGameSelect = (game) => {
-        setActiveGame(game);
-    };
+  const handleGameSelect = (game) => {
+    setActiveGame(game);
+    console.log(game);
+  };
 
-    return (
-      <div className="choose-game">
-        <PageTopBar />
-        <div className="background-choose-game">
-          <img src="src/components/choose-game/choose-game-images/background.png" className="background-img" />
-          <img src="src/components/choose-game/choose-game-images/Pink-Background.png" className="Pink-Background" />
-        </div>
+  return (
+    <div className="choose-game">
+      <PageTopBar />
+      <div className="background-choose-game">
         <div className="games">
-          <img src="src/components/choose-game/choose-game-images/Go-Groceries-Selected.png" className={`game-image ${activeGame === 'Go-Groceries' ? 'active' : ''}`} onClick={() => handleGameSelect('Go-Groceries')} />
-          <img src="src/components/choose-game/choose-game-images/Directions-Unselected.png" className={`game-image ${activeGame === 'Go-Groceries' ? 'active' : ''}`} onClick={() => handleGameSelect('Directions')} />
-          <img src="src/components/choose-game/choose-game-images/Orderly-Chaos-Unselected.png" className={`game-image ${activeGame === 'Go-Groceries' ? 'active' : ''}`} onClick={() => handleGameSelect('Orderly-Chaos')} />
+          <img
+            src={gogogroceriesselected}
+            className="game-image"
+            onClick={() => handleGameSelect('Go-Groceries')}
+          />
+          <img
+            src={directionsunselected}
+            className="game-image"
+            id="unavail"
+            onClick={() => handleGameSelect('Directions')}
+          />
+          <img
+            src={orderlychaosunselected}
+            className="game-image"
+            id="unavail"
+            onClick={() => handleGameSelect('Orderly-Chaos')}
+          />
         </div>
         <div className="players">
-          <img src="src/components/choose-game/choose-game-images/red-player.png" className="red-player" />
-          <img src="src/components/choose-game/choose-game-images/blue-player.png" className="blue-player" />
-          <img src="src/components/choose-game/choose-game-images/yellow-player.png" className="yellow-player" />
-          <img src="src/components/choose-game/choose-game-images/green-player.png" className="green-player" />
+          <img src={redplayer} className="red-player" />
+          <img src={blueplayer} className="blue-player" />
+          <img src={yellowplayer} className="yellow-player" />
+          <img src={greenplayer} className="green-player" />
         </div>
-        <div className="pengu">
-          <img src="src/components/choose-game/choose-game-images/Pengu Happy.png" className="pengu-img" />
-        </div>
-        <img src="src/components/choose-game/choose-game-images/select-game.png" className="select-game" />
-        <img src="src/components/choose-game/choose-game-images/groceries-description.png" className="groceries-description" />
+        <img src={penguhappy} className="pengu-img" />
+        <img src={selectgame} className="select-game" />
+        <img src={groceriesdescription} className="groceries-description" />
 
-        <NavLink to="/creategame"><img src="src/components/choose-game/choose-game-images/Group 49.png" alt="Back" className="back-button" /></NavLink>
-
+        <button
+          type="button"
+          className="back-button"
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          <img src={backbutton} alt="back loading icon" />
+        </button>
       </div>
-    );
+    </div>
+  );
 }
 
 export default ChooseGame;
