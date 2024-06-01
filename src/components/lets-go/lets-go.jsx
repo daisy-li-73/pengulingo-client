5e999eab5102cc3421b75e76d96777d348c4b2c6
Selena Zhou <58157434+zhouselena@users.noreply.github.com>
2024-06-01 12:19:53 -0400

    fix lets go page styling

--- src/components/lets-go/lets-go.jsx
+++ src/components/lets-go/lets-go.jsx
@@ -1,59 +1,123 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable react/jsx-indent */
 /* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable react/function-component-definition */
 import React from 'react';
-import PageTopBar from '../PageTopBar';
 
-const LetsGo = () => {
+function LetsGo() {
   return (
-        <div className="lets-go-class">
-            <PageTopBar />
-            <div className="lights">
-                <img src="src/components/lets-go/images-lets-go/Pink Light.png" className="P1Light" />
-                <img src="src/components/lets-go/images-lets-go/Blue Light.png" className="P2Light" />
-                <img src="src/components/lets-go/images-lets-go/Yellow Light.png" className="P3Light" />
-                <img src="src/components/lets-go/images-lets-go/Green Light.png" className="P4Light" />
-            </div>
-            <div className="players">
-                <img src="src/components/lets-go/images-lets-go/P1.png" className="P1" />
-                <img src="src/components/lets-go/images-lets-go/P2.png" className="P2" />
-                <img src="src/components/lets-go/images-lets-go/P3.png" className="P3" />
-                <img src="src/components/lets-go/images-lets-go/P4.png" className="P4" />
-            </div>
-            <div className="pengu">
-                <img src="src/components/lets-go/images-lets-go/Pengu Happy.png" className="pengu-img" />
-            </div>
-            <div className="circle">
-                <img src="src/components/lets-go/images-lets-go/Ellipse 15.png" className="ellipse" />
-            </div>
-            <div className="lets-go-text">
-                <img src="src/components/lets-go/images-lets-go/lets-go.png" className="lets-go-img" />
-            </div>
-            <div className="sparkles">
-                <div className="red-sparkles">
-                    <img src="src/components/lets-go/images-lets-go/Star 6.png" className="star-6" />
-                    <img src="src/components/lets-go/images-lets-go/Star 7.png" className="star-7" />
-                    <img src="src/components/lets-go/images-lets-go/Star 8.png" className="star-8" />
-                </div>
-                <div className="blue-sparkles">
-                    <img src="src/components/lets-go/images-lets-go/Star 9.png" className="star-9" />
-                    <img src="src/components/lets-go/images-lets-go/Star 10.png" className="star-10" />
-                    <img src="src/components/lets-go/images-lets-go/Star 11.png" className="star-11" />
-                </div>
-                <div className="yellow-sparkles">
-                    <img src="src/components/lets-go/images-lets-go/Star 12.png" className="star-12" />
-                    <img src="src/components/lets-go/images-lets-go/Star 13.png" className="star-13" />
-                    <img src="src/components/lets-go/images-lets-go/Star 14.png" className="star-14" />
-                </div>
-                <div className="green-sparkles">
-                    <img src="src/components/lets-go/images-lets-go/Star 15.png" className="star-15" />
-                    <img src="src/components/lets-go/images-lets-go/Star 16.png" className="star-16" />
-                    <img src="src/components/lets-go/images-lets-go/Star 17.png" className="star-17" />
-                </div>
-            </div>
+    <div className="lets-go-class">
+      <div className="lights">
+        <img
+          src="src/components/lets-go/images-lets-go/Pink Light.png"
+          className="P1Light"
+        />
+        <img
+          src="src/components/lets-go/images-lets-go/Blue Light.png"
+          className="P2Light"
+        />
+        <img
+          src="src/components/lets-go/images-lets-go/Yellow Light.png"
+          className="P3Light"
+        />
+        <img
+          src="src/components/lets-go/images-lets-go/Green Light.png"
+          className="P4Light"
+        />
+      </div>
+      <div className="players">
+        <img
+          src="src/components/lets-go/images-lets-go/P1.png"
+          className="P1"
+        />
+        <img
+          src="src/components/lets-go/images-lets-go/P2.png"
+          className="P2"
+        />
+        <img
+          src="src/components/lets-go/images-lets-go/P3.png"
+          className="P3"
+        />
+        <img
+          src="src/components/lets-go/images-lets-go/P4.png"
+          className="P4"
+        />
+      </div>
+      <div className="pengu">
+        <img
+          src="src/components/lets-go/images-lets-go/Pengu Happy.png"
+          className="pengu-img"
+        />
+        <div className="lets-go-text">
+          <img
+            src="src/components/lets-go/images-lets-go/lets-go.png"
+            className="lets-go-img"
+          />
         </div>
+      </div>
+      <div className="circle">
+        <img
+          src="src/components/lets-go/images-lets-go/Ellipse 15.png"
+          className="ellipse"
+        />
+      </div>
+      <div className="sparkles">
+        <div className="red-sparkles">
+          <img
+            src="src/components/lets-go/images-lets-go/Star 6.png"
+            className="star-6"
+          />
+          <img
+            src="src/components/lets-go/images-lets-go/Star 7.png"
+            className="star-7"
+          />
+          <img
+            src="src/components/lets-go/images-lets-go/Star 8.png"
+            className="star-8"
+          />
+        </div>
+        <div className="blue-sparkles">
+          <img
+            src="src/components/lets-go/images-lets-go/Star 9.png"
+            className="star-9"
+          />
+          <img
+            src="src/components/lets-go/images-lets-go/Star 10.png"
+            className="star-10"
+          />
+          <img
+            src="src/components/lets-go/images-lets-go/Star 11.png"
+            className="star-11"
+          />
+        </div>
+        <div className="yellow-sparkles">
+          <img
+            src="src/components/lets-go/images-lets-go/Star 12.png"
+            className="star-12"
+          />
+          <img
+            src="src/components/lets-go/images-lets-go/Star 13.png"
+            className="star-13"
+          />
+          <img
+            src="src/components/lets-go/images-lets-go/Star 14.png"
+            className="star-14"
+          />
+        </div>
+        <div className="green-sparkles">
+          <img
+            src="src/components/lets-go/images-lets-go/Star 15.png"
+            className="star-15"
+          />
+          <img
+            src="src/components/lets-go/images-lets-go/Star 16.png"
+            className="star-16"
+          />
+          <img
+            src="src/components/lets-go/images-lets-go/Star 17.png"
+            className="star-17"
+          />
+        </div>
+      </div>
+    </div>
   );
-};
+}
 
 export default LetsGo;
